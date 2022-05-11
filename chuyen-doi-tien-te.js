049ebfb737f95d292351dc4a94ff9c324b69ad4c
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-05-11 15:59:17 +0700

    chuyen-tien

---
+++ chuyen-doi-tien-te.js
@@ -0,0 +1,14 @@
+function ChangeMoney() {
+    let Amount = document.getElementById("Amount").value;
+    let From = document.getElementById("From").value;
+    let To = document.getElementById("To").value;
+    let Result;
+    if (From=="USD"&&To=="VND"){
+        Result = "Result: " + (Amount * 23000) ;
+    }
+    else if (From=="VND"&&To=="USD"){
+        Result = "Result: " + (Amount / 23000) ;
+    }
+    alert(Result);
+
+}
